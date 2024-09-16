48ab9b0758f28540711c82e636e4cfe90114eea9
lopatop <pavellopato327@gmail.com>
2024-09-16 18:38:48 +0200

    lesson02-03deletAlert

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -9,22 +9,19 @@ let grade
 // your code
 if (score <= 49 && score > 0) {
     grade = 'F'
-    document.write(grade)
 } else if (score <= 69 && score >= 50) {
     grade = 'D'
-    document.write(grade)
 } else if (score <= 79 && score >= 70) {
     grade = 'C'
-    document.write(grade)
 } else if (score <= 89 && score >= 80) {
     grade = 'B'
-    document.write(grade)
 } else if (score <= 100 && score >= 90) {
     grade = 'A'
-    document.write(grade)
 }
     else {
-        alert ('Введите число от 0 до 100');
-
+        console.log('Введите число от 0 до 100');
     }
+if (grade){
+    document.write(grade);
+}
 
