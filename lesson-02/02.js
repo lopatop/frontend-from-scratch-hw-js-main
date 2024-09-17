1fc1604631db4af019098af71583978a401522b0
lopatop <pavellopato327@gmail.com>
2024-09-17 19:45:01 +0200

    lesson02-02

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,20 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.floor(Math.random()*7);
+let dice2 = Math.floor(Math.random()*7);
+let isWinningDouble; 
+const numbers = 3; 
+
+console.log('Первый бросок: ' + dice1)
+console.log('Второй бросок: ' + dice2)
+if (dice1 === dice2 && dice1 > numbers) {
+  isWinningDouble === true;
+  console.log('Выигрышный дубль!')
+} else {
+    isWinningDouble === false; 
+  console.log('Не выигрышный дубль.')
+}
 
 /*
 console.log('Первый бросок: ' + dice1)
