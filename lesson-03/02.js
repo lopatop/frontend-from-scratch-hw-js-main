ce6b28597e11d6721d56c877790b48e386a01750
lopatop <pavellopato327@gmail.com>
2024-09-23 18:12:56 +0200

    lesson03-02

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,16 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+let number = +prompt('Введите число');
+
+function isEven(a) {
+    if (a % 2 === 0) {
+        return true;
+
+    } else {
+        return false
+    }
+}
+let result = isEven(number);
+console.log(result);
+
