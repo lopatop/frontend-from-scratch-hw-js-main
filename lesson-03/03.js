691081df6ed27844a88aace94324329c4cba9d41
lopatop <pavellopato327@gmail.com>
2024-09-24 18:11:34 +0200

    lesson03-03

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,1 +1,22 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+
+const number1 = +prompt('Введите первое число');
+const number2 = +prompt('Введите второе число');
+const number3 = +prompt('Введите третье число');
+
+function findLargest(a, b, c) {
+
+    if (a > b && a > c) {
+        console.log(a);
+        return a; 
+    } else if (b > a && b > c) {
+        console.log(b);
+        return b;
+    } else {
+        console.log(c);
+        return c; 
+    }
+
+}
+
+const morNumber = findLargest(number1, number2, number3);
