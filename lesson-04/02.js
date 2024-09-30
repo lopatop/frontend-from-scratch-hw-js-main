c3c013d316d86164ca4ceb769eabc7be17b3d19a
lopatop <pavellopato327@gmail.com>
2024-09-30 22:15:34 +0200

    function findUniqueElements

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,3 +15,16 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
+
+function findUniqueElements(array) {
+    const uniqueElements = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+
+        if (!includesElement(uniqueElements, element)) {
+            uniqueElements.push(element);
+        }
+    }
+    return uniqueElements;
+}
