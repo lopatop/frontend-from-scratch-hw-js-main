0c0b5e1f1a8665e0a5f125e1ab15aed5a6ad8a40
lopatop <pavellopato327@gmail.com>
2024-10-01 23:37:00 +0200

    lesson04-03-01

--- lesson-04/02.js
+++ lesson-04/02.js
@@ -15,15 +15,6 @@
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function includesElement(array, element) {
-
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] === element) {
-            return true;
-        }
-    }
-    return false;
-}
 
 
 function findUniqueElements(array) {
--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,6 +17,16 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
+function includesElement(array, element) {
+
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true;
+        }
+    }
+    return false;
+}
+
 function findCommonElements(array1, array2) {
     const commonElements = [];
     for (let i = 0; i < array1.length; i++) {
